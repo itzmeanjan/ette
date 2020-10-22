d4282ec60a56f1e3959afa578d0d509cae32fb49
Anjan Roy <anjanroy@yandex.com>
2020-10-22 22:35:20 +0530

    reading .env file in

---
+++ config/config.go
@@ -0,0 +1,15 @@
+package config
+
+import "github.com/spf13/viper"
+
+// Reading .env file content, during application start up
+func read(file string) error {
+	viper.SetConfigFile(file)
+
+	return viper.ReadInConfig()
+}
+
+// Get config value by key
+func get(key string) string {
+	return viper.GetString(key)
+}
