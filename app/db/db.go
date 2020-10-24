1ba3a9a2179b5ad7bbfe7af423a56b5cca54eee9
Anjan Roy <anjanroy@yandex.com>
2020-10-24 13:53:33 +0530

    db object model

--- app/db/db.go
+++ app/db/db.go
@@ -8,25 +8,25 @@ import (
 
 // Blocks - ...
 type Blocks struct {
-	Hash       common.Hash
-	Number     *big.Int
-	Time       uint64
-	ParentHash common.Hash
-	Difficulty *big.Int
-	GasUsed    uint64
-	GasLimit   uint64
-	Nonce      uint64
+	Hash       string `gorm:"column:hash;type:char(66);primaryKey"`
+	Number     string `gorm:"column:number;type:varchar;not null"`
+	Time       uint64 `gorm:"column:time;type:bigint;not null"`
+	ParentHash string `gorm:"column:parenthash;type:char(66);not null"`
+	Difficulty string `gorm:"column:difficulty;type:varchar;not null"`
+	GasUsed    uint64 `gorm:"column:gasused;type:bigint;not null"`
+	GasLimit   uint64 `gorm:"column:gaslimit;type:bigint;not null"`
+	Nonce      uint64 `gorm:"column:nonce;type:bigint;not null"`
 }
 
 // Transactions - ...
 type Transactions struct {
-	Hash      common.Hash
-	From      common.Address
-	To        common.Address
-	Gas       uint64
-	GasPrice  *big.Int
-	Cost      *big.Int
-	Nonce     uint64
-	State     uint8
-	BlockHash common.Hash
+	Hash      common.Hash    `gorm:"column:hash;type:char(66);primaryKey"`
+	From      common.Address `gorm:"column:from;type:char(42);not null"`
+	To        common.Address `gorm:"column:to;type:char(42); not null"`
+	Gas       uint64         `gorm:"column:gas;type:bigint;not null"`
+	GasPrice  *big.Int       `gorm:"column:gasprice;type:varchar;not null"`
+	Cost      *big.Int       `gorm:"column:cost;type:varchar;not null"`
+	Nonce     uint64         `gorm:"column:nonce;type:bigint;not null"`
+	State     uint8          `gorm:"column:state;type:smallint;not null"`
+	BlockHash common.Hash    `gorm:"column:blockhash;type:char(66);not null"`
 }
