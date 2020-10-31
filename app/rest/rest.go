993a3207522771293e8aea70c043d806cca5cd40
Anjan Roy <anjanroy@yandex.com>
2020-10-31 08:56:55 +0530

    GET endpoint for accepting block hash & returning block data

--- app/rest/rest.go
+++ app/rest/rest.go
@@ -2,11 +2,14 @@ package rest
 
 import (
 	"fmt"
+	"net/http"
 	"sync"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/gin-gonic/gin"
 	cfg "github.com/itzmeanjan/ette/app/config"
 	d "github.com/itzmeanjan/ette/app/data"
+	"github.com/itzmeanjan/ette/app/db"
 	"gorm.io/gorm"
 )
 
@@ -23,8 +26,22 @@ func RunHTTPServer(_db *gorm.DB, _lock *sync.Mutex, _synced *d.SyncState) {
 			_lock.Lock()
 			defer _lock.Unlock()
 
-			c.JSON(200, gin.H{
+			c.JSON(http.StatusOK, gin.H{
 				"synced": _synced.Synced,
+			})
+
+		})
+
+		// Query block data using block hash
+		grp.GET("/block/:hash", func(c *gin.Context) {
+
+			if block := db.GetBlockByHash(_db, common.HexToHash(c.Param("hash"))); block != nil {
+				c.Data(http.StatusOK, "application/json", block.ToJSON())
+				return
+			}
+
+			c.JSON(http.StatusNoContent, gin.H{
+				"msg": "Bad block hash",
 			})
 
 		})
