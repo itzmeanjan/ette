8eb482519138e97f4d9df992967ba80c454e9f54
Anjan Roy <anjanroy@yandex.com>
2020-10-31 09:14:11 +0530

    fetch block by number

--- app/db/query.go
+++ app/db/query.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"strconv"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/itzmeanjan/ette/app/data"
 	"gorm.io/gorm"
@@ -13,6 +15,24 @@ func GetBlockByHash(db *gorm.DB, hash common.Hash) *data.Block {
 	var block data.Block
 
 	if res := db.Model(&Blocks{}).Where("hash = ?", hash.Hex()).First(&block); res.Error != nil {
+		return nil
+	}
+
+	return &block
+}
+
+// GetBlockByNumber - Fetch block using block number
+//
+// If not found, returns nil
+func GetBlockByNumber(db *gorm.DB, number string) *data.Block {
+	_num, err := strconv.Atoi(number)
+	if err != nil {
+		return nil
+	}
+
+	var block data.Block
+
+	if res := db.Model(&Blocks{}).Where("number = ?", _num).First(&block); res.Error != nil {
 		return nil
 	}
 
