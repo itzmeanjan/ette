2c707a51746de4791236c9cfe2e333cdac4ea40c
Anjan Roy <anjanroy@yandex.com>
2020-10-31 08:49:31 +0530

    block info query data to be supplied in different struct

--- app/data/data.go
+++ app/data/data.go
@@ -4,3 +4,15 @@ package data
 type SyncState struct {
 	Synced bool
 }
+
+// Block - Block related info to be delivered to client in this format
+type Block struct {
+	Hash       string `json:"hash"`
+	Number     uint64 `json:"number"`
+	Time       uint64 `json:"time"`
+	ParentHash string `json:"parentHash"`
+	Difficulty string `json:"difficulty"`
+	GasUsed    uint64 `json:"gasUsed"`
+	GasLimit   uint64 `json:"gasLimit"`
+	Nonce      uint64 `json:"nonce"`
+}
--- app/db/query.go
+++ app/db/query.go
@@ -2,14 +2,15 @@ package db
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/itzmeanjan/ette/app/data"
 	"gorm.io/gorm"
 )
 
 // GetBlockByHash - Given blockhash finds out block related information
 //
 // If not found, returns nil
-func GetBlockByHash(db *gorm.DB, hash common.Hash) *Blocks {
-	var block Blocks
+func GetBlockByHash(db *gorm.DB, hash common.Hash) *data.Block {
+	var block data.Block
 
 	if res := db.Model(&Blocks{}).Where("hash = ?", hash.Hex()).First(&block); res.Error != nil {
 		return nil
