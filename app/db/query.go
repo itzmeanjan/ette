5a81f0b5861250184a0a5eede9e1bd57032ca51e
Anjan Roy <anjanroy@yandex.com>
2020-11-01 20:53:10 +0530

    fetch all tx from blocks mined in given time range & where `to` field of tx(s) are as given in filtering condition

--- app/db/query.go
+++ app/db/query.go
@@ -147,3 +147,17 @@ func GetTransactionsToAccountByBlockNumberRange(db *gorm.DB, account common.Addr
 		Transactions: tx,
 	}
 }
+
+// GetTransactionsToAccountByBlockTimeRange - Given account address which is present in `to` field of tx(s)
+// held in blocks mined with in given time range
+func GetTransactionsToAccountByBlockTimeRange(db *gorm.DB, account common.Address, from uint64, to uint64) *data.Transactions {
+	var tx []*data.Transaction
+
+	if err := db.Model(&Transactions{}).Joins("left join blocks on transactions.blockhash = blocks.hash").Where("transactions.to = ? and blocks.time >= ? and blocks.time <= ?", account.Hex(), from, to).Select("transactions.hash, transactions.from, transactions.to, transactions.contract, transactions.gas, transactions.gasprice, transactions.cost, transactions.nonce, transactions.state, transactions.blockhash").Find(&tx).Error; err != nil {
+		return nil
+	}
+
+	return &data.Transactions{
+		Transactions: tx,
+	}
+}
