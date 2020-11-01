e822460fb76d85f269ceee8de53d7a0c3ba0066d
Anjan Roy <anjanroy@yandex.com>
2020-11-01 20:48:35 +0530

    fetch all tx where account is in `to` field & block number range is specified

--- app/db/query.go
+++ app/db/query.go
@@ -133,3 +133,17 @@ func GetTransactionsFromAccountByBlockTimeRange(db *gorm.DB, account common.Addr
 		Transactions: tx,
 	}
 }
+
+// GetTransactionsToAccountByBlockNumberRange - Given account address & block number range, returns transactions where
+// `account` was in `to` field
+func GetTransactionsToAccountByBlockNumberRange(db *gorm.DB, account common.Address, from uint64, to uint64) *data.Transactions {
+	var tx []*data.Transaction
+
+	if err := db.Model(&Transactions{}).Joins("left join blocks on transactions.blockhash = blocks.hash").Where("transactions.to = ? and blocks.number >= ? and blocks.number <= ?", account.Hex(), from, to).Select("transactions.hash, transactions.from, transactions.to, transactions.contract, transactions.gas, transactions.gasprice, transactions.cost, transactions.nonce, transactions.state, transactions.blockhash").Find(&tx).Error; err != nil {
+		return nil
+	}
+
+	return &data.Transactions{
+		Transactions: tx,
+	}
+}
