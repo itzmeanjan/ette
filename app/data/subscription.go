1d60f26acb2199d16e8d65600216f7b6d3c41bb1
Anjan Roy <anjanroy@yandex.com>
2020-11-13 20:15:03 +0530

    checking whether client has really shown interest in receiving notification for this tx or not

--- app/data/subscription.go
+++ app/data/subscription.go
@@ -51,6 +51,59 @@ func (s *SubscriptionRequest) GetTransactionFilters() []string {
 	return []string{matches[4], matches[6]}
 }
 
+// DoesMatchWithPubslishedTransactionData - All `transaction` topic listeners i.e. subscribers are
+// going to get notified when new transaction detected, but they will only send those data to client application
+// ( connected over websocket ), to which client has subscribed to
+//
+// Whether client has really shown interest in receiving notification for this transaction or not
+// can be checked using this function
+func (s *SubscriptionRequest) DoesMatchWithPubslishedTransactionData(tx *Transaction) bool {
+
+	// --- This closure function tries to match with to field of published tx data
+	//
+	// to field might not be present in some tx(s), where contract get deployed
+	matchToFieldInTx := func(to string) bool {
+		status := false
+
+		switch to {
+		// match with any `to` address
+		case "":
+			status = true
+		// match with any `to` address
+		case "*":
+			status = true
+		// match with specific `to` address
+		default:
+			if to == tx.To {
+				status = true
+			}
+		}
+
+		return status
+	}
+	// ---
+
+	// Fetches desired to & from fields, against which matching to be performed
+	filters := s.GetTransactionFilters()
+	status := false
+
+	switch filters[0] {
+	// match with any `from` address
+	case "":
+		status = matchToFieldInTx(filters[1])
+	// match with any `from` address
+	case "*":
+		status = matchToFieldInTx(filters[1])
+	// match with provided `from` address
+	default:
+		if filters[0] == tx.From {
+			status = matchToFieldInTx(filters[1])
+		}
+	}
+
+	return status
+}
+
 // IsValidTopic - Checks whether topic to which client application is trying to
 // subscribe to is valid one or not
 func (s *SubscriptionRequest) IsValidTopic() bool {
