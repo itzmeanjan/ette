7c54e90b89a98d3a8be962bd30b6b2851ed02fa3
Anjan Roy <anjanroy@yandex.com>
2020-11-16 00:16:53 +0530

    example js listener over websocket, for receiving notification for all events emitted by smart contract at address 0x...

---
+++ example/event_2.js
@@ -0,0 +1,31 @@
+const { client } = require('websocket')
+
+const _client = new client()
+
+let state = true
+
+_client.on('connectFailed', e => { console.error(`[!] Failed to connect : ${e}`); process.exit(1); })
+
+// For listening to any event being emitted
+// by smart contract at `0xcb3fA413B23b12E402Cfcd8FA120f983FB70d8E8`
+_client.on('connect', c => {
+    c.on('close', d => {
+        console.log(`[!] Closed connection : ${d}`)
+        process.exit(0)
+    })
+
+    c.on('message', d => { console.log(JSON.parse(d.utf8Data)) })
+
+    handler = _ => {
+
+        c.send(JSON.stringify({ name: 'event/0xcb3fA413B23b12E402Cfcd8FA120f983FB70d8E8/*/*/*/*', type: state ? 'subscribe' : 'unsubscribe' }))
+        state = !state
+
+    }
+
+    setInterval(handler, 10000)
+    handler()
+
+})
+
+_client.connect('ws://localhost:7000/v1/ws', null)
