62d0127bee5c6451e4089571d65e96ffea315c91
Anjan Roy <anjanroy@yandex.com>
2020-11-20 01:21:22 +0530

    computing new API key for user account

--- app/db/users.go
+++ app/db/users.go
@@ -1,7 +1,11 @@
 package db
 
 import (
+	"encoding/json"
+	"time"
+
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"gorm.io/gorm"
 )
 
@@ -15,4 +19,40 @@ func GetAppsByUserAddress(_db *gorm.DB, address common.Address) []*Users {
 	}
 
 	return apps
+}
+
+// ComputeAPIKeyForAddress - Computing new API key for user address, by taking nonce of that account under consideration
+//
+// Here nonce is nothing but count of applications created in `ette` by account
+func ComputeAPIKeyForAddress(_db *gorm.DB, address common.Address) []byte {
+	var count int64
+
+	if err := _db.Model(&Users{}).Where("users.address = ?", address.Hex()).Count(&count).Error; err != nil {
+		return nil
+	}
+
+	data, err := json.Marshal(&struct {
+		Address common.Address `json:"address"`
+		Nonce   int64          `json:"nonce"`
+	}{
+		Address: address,
+		Nonce:   count + 1,
+	})
+	if err != nil {
+		return nil
+	}
+
+	return crypto.Keccak256(data)
+}
+
+// RegisterNewApp - ...
+func RegisterNewApp(_db *gorm.DB, address common.Address) bool {
+	if err := _db.Create(&Users{
+		Address:   address,
+		TimeStamp: time.Now(),
+	}).Error; err != nil {
+		return false
+	}
+
+	return true
 }
