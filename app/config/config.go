5c5a66b23a23fe0c8b1a00d3a4ace03b1cd431ee
Anjan Roy <anjanroy@yandex.com>
2021-01-05 21:04:59 +0530

    read concurrency factor from config file & return as unsigned integer

--- app/config/config.go
+++ app/config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"log"
+	"strconv"
+
+	"github.com/spf13/viper"
+)
 
 // Read - Reading .env file content, during application start up
 func Read(file string) error {
@@ -12,4 +17,22 @@ func Read(file string) error {
 // Get - Get config value by key
 func Get(key string) string {
 	return viper.GetString(key)
+}
+
+// GetConcurrencyFactor - Reads concurrency factor specified in `.env` file, during deployment
+// and returns that number as unsigned integer
+func GetConcurrencyFactor() uint64 {
+
+	factor := Get("ConcurrencyFactor")
+	if factor == "" {
+		return 1
+	}
+
+	parsedFactor, err := strconv.ParseUint(factor, 10, 64)
+	if err != nil {
+		log.Printf("[!] Failed to parse concurrency factor : %s\n", err.Error())
+		return 1
+	}
+
+	return parsedFactor
 }
