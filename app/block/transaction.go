e2b0c66ea4207136d3d2b226d1c18c6c63b89563
Anjan Roy <anjanroy@yandex.com>
2021-01-06 23:24:47 +0530

    removed persistance functionality from tx fetcher, more functions, doing only their job

--- app/block/transaction.go
+++ app/block/transaction.go
@@ -15,8 +15,11 @@ import (
 	"gorm.io/gorm"
 )
 
-// FetchTransactionByHash - Fetching specific transaction related data & persisting in database
+// FetchTransactionByHash - Fetching specific transaction related data, tries to publish data if required
+// & lets listener go routine know about all tx, event data it collected while processing this tx,
+// which will be attempted to be stored in database
 func FetchTransactionByHash(client *ethclient.Client, block *types.Block, tx *types.Transaction, _db *gorm.DB, redis *d.RedisInfo, publishable bool, _status *d.StatusHolder, returnValChan chan *db.PackedTransaction) {
+
 	receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
 	if err != nil {
 		log.Print(color.Red.Sprintf("[!] Failed to fetch tx receipt [ block : %d ] : %s", block.NumberU64(), err.Error()))
@@ -37,85 +40,32 @@ func FetchTransactionByHash(client *ethclient.Client, block *types.Block, tx *ty
 		return
 	}
 
-	// Packing tx related data inside one struct
-	packedTx := BuildPackedTx(tx, sender, receipt)
+	// Passing all tx related data to listener go routine
+	// so that it can attempt to store whole block data
+	// into database
+	defer func() {
+		returnValChan <- BuildPackedTx(tx, sender, receipt)
+	}()
 
 	// This is not a case when real time data is received, rather this is probably
 	// a sync attempt to latest state of blockchain
 	//
 	// So, in this case, we don't need to publish any data on pubsub channel
 	if !publishable {
-
-		// Passing all tx related data to listener go routine
-		// so that it can attempt to store whole block data
-		// into database
-		returnValChan <- packedTx
 		return
-
 	}
 
+	// Checking whether `ette` is being run in real-time data delivery
+	// mode or not
+	//
+	// If yes & this tx, event log data can be published,
+	// we'll try to publish data to redis pubsub channel
+	// which will be eventually broadcasted to all clients subscribed to
+	// topic of their interest
 	if cfg.Get("EtteMode") == "2" || cfg.Get("EtteMode") == "3" {
-
-		var _publishTx *d.Transaction
-
-		if tx.To() == nil {
-			// This is a contract creation tx
-			_publishTx = &d.Transaction{
-				Hash:      tx.Hash().Hex(),
-				From:      sender.Hex(),
-				Contract:  receipt.ContractAddress.Hex(),
-				Value:     tx.Value().String(),
-				Data:      tx.Data(),
-				Gas:       tx.Gas(),
-				GasPrice:  tx.GasPrice().String(),
-				Cost:      tx.Cost().String(),
-				Nonce:     tx.Nonce(),
-				State:     receipt.Status,
-				BlockHash: receipt.BlockHash.Hex(),
-			}
-		} else {
-			// This is a normal tx, so we keep contract field empty
-			_publishTx = &d.Transaction{
-				Hash:      tx.Hash().Hex(),
-				From:      sender.Hex(),
-				To:        tx.To().Hex(),
-				Value:     tx.Value().String(),
-				Data:      tx.Data(),
-				Gas:       tx.Gas(),
-				GasPrice:  tx.GasPrice().String(),
-				Cost:      tx.Cost().String(),
-				Nonce:     tx.Nonce(),
-				State:     receipt.Status,
-				BlockHash: receipt.BlockHash.Hex(),
-			}
-		}
-
-		if err := redis.Client.Publish(context.Background(), "transaction", _publishTx).Err(); err != nil {
-			log.Print(color.Red.Sprintf("[!] Failed to publish transaction from block %d : %s", block.NumberU64(), err.Error()))
-		}
-
-		// Publishing event/ log entries to redis pub-sub topic, to be captured by subscribers
-		// and sent to client application, who are interested in this piece of data
-		// after applying filter
-		for _, v := range receipt.Logs {
-
-			if err := redis.Client.Publish(context.Background(), "event", &d.Event{
-				Origin:          v.Address.Hex(),
-				Index:           v.Index,
-				Topics:          c.StringifyEventTopics(v.Topics),
-				Data:            v.Data,
-				TransactionHash: v.TxHash.Hex(),
-				BlockHash:       v.BlockHash.Hex(),
-			}).Err(); err != nil {
-				log.Print(color.Red.Sprintf("[!] Failed to publish event from block %d : %s", block.NumberU64(), err.Error()))
-			}
-
-		}
-
+		PublishTx(block.NumberU64(), tx, sender, receipt, redis)
 	}
 
-	// Notifying listener go routine, about status of this executing thread
-	returnValChan <- packedTx
 }
 
 // BuildPackedTx - Putting all information, `ette` will keep for one tx
@@ -174,5 +124,67 @@ func BuildPackedTx(tx *types.Transaction, sender common.Address, receipt *types.
 	}
 
 	return packedTx
+
+}
+
+// PublishTx - Publishes tx & events in tx, related data to respective
+// Redis pubsub channel
+func PublishTx(blockNumber uint64, tx *types.Transaction, sender common.Address, receipt *types.Receipt, redis *d.RedisInfo) {
+
+	var pTx *d.Transaction
+
+	if tx.To() == nil {
+		// This is a contract creation tx
+		pTx = &d.Transaction{
+			Hash:      tx.Hash().Hex(),
+			From:      sender.Hex(),
+			Contract:  receipt.ContractAddress.Hex(),
+			Value:     tx.Value().String(),
+			Data:      tx.Data(),
+			Gas:       tx.Gas(),
+			GasPrice:  tx.GasPrice().String(),
+			Cost:      tx.Cost().String(),
+			Nonce:     tx.Nonce(),
+			State:     receipt.Status,
+			BlockHash: receipt.BlockHash.Hex(),
+		}
+	} else {
+		// This is a normal tx, so we keep contract field empty
+		pTx = &d.Transaction{
+			Hash:      tx.Hash().Hex(),
+			From:      sender.Hex(),
+			To:        tx.To().Hex(),
+			Value:     tx.Value().String(),
+			Data:      tx.Data(),
+			Gas:       tx.Gas(),
+			GasPrice:  tx.GasPrice().String(),
+			Cost:      tx.Cost().String(),
+			Nonce:     tx.Nonce(),
+			State:     receipt.Status,
+			BlockHash: receipt.BlockHash.Hex(),
+		}
+	}
+
+	if err := redis.Client.Publish(context.Background(), "transaction", pTx).Err(); err != nil {
+		log.Print(color.Red.Sprintf("[!] Failed to publish transaction from block %d : %s", blockNumber, err.Error()))
+	}
+
+	// Publishing event/ log entries to redis pub-sub topic, to be captured by subscribers
+	// and sent to client application, who are interested in this piece of data
+	// after applying filter
+	for _, v := range receipt.Logs {
+
+		if err := redis.Client.Publish(context.Background(), "event", &d.Event{
+			Origin:          v.Address.Hex(),
+			Index:           v.Index,
+			Topics:          c.StringifyEventTopics(v.Topics),
+			Data:            v.Data,
+			TransactionHash: v.TxHash.Hex(),
+			BlockHash:       v.BlockHash.Hex(),
+		}).Err(); err != nil {
+			log.Print(color.Red.Sprintf("[!] Failed to publish event from block %d : %s", blockNumber, err.Error()))
+		}
+
+	}
 
 }
