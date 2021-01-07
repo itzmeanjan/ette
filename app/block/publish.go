eefbb13e6b257f1cb8f13f11c135f568c7e2ccb9
Anjan Roy <anjanroy@yandex.com>
2021-01-07 22:11:35 +0530

    publishing to pubsub topic handler functions

--- app/block/publish.go
+++ app/block/publish.go
@@ -4,70 +4,68 @@ import (
 	"context"
 	"log"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/gookit/color"
-	c "github.com/itzmeanjan/ette/app/common"
 	d "github.com/itzmeanjan/ette/app/data"
+	"github.com/itzmeanjan/ette/app/db"
 )
 
 // PublishBlock - Attempts to publish block data to Redis pubsub channel
-func PublishBlock(block *types.Block, redis *d.RedisInfo) {
+func PublishBlock(block *db.Blocks, redis *d.RedisInfo) {
 
 	if err := redis.Client.Publish(context.Background(), "block", &d.Block{
-		Hash:                block.Hash().Hex(),
-		Number:              block.NumberU64(),
-		Time:                block.Time(),
-		ParentHash:          block.ParentHash().Hex(),
-		Difficulty:          block.Difficulty().String(),
-		GasUsed:             block.GasUsed(),
-		GasLimit:            block.GasLimit(),
-		Nonce:               block.Nonce(),
-		Miner:               block.Coinbase().Hex(),
-		Size:                float64(block.Size()),
-		TransactionRootHash: block.TxHash().Hex(),
-		ReceiptRootHash:     block.ReceiptHash().Hex(),
+		Hash:                block.Hash,
+		Number:              block.Number,
+		Time:                block.Time,
+		ParentHash:          block.ParentHash,
+		Difficulty:          block.Difficulty,
+		GasUsed:             block.GasUsed,
+		GasLimit:            block.GasLimit,
+		Nonce:               block.Nonce,
+		Miner:               block.Miner,
+		Size:                block.Size,
+		TransactionRootHash: block.TransactionRootHash,
+		ReceiptRootHash:     block.ReceiptRootHash,
 	}).Err(); err != nil {
-		log.Print(color.Red.Sprintf("[!] Failed to publish block %d in channel : %s", block.NumberU64(), err.Error()))
+		log.Print(color.Red.Sprintf("[!] Failed to publish block %d in channel : %s", block.Number, err.Error()))
 	}
 
 }
 
 // PublishTx - Publishes tx & events in tx, related data to respective
 // Redis pubsub channel
-func PublishTx(blockNumber uint64, tx *types.Transaction, sender common.Address, receipt *types.Receipt, redis *d.RedisInfo) {
+func PublishTx(blockNumber uint64, tx *db.Transactions, redis *d.RedisInfo) {
 
 	var pTx *d.Transaction
 
-	if tx.To() == nil {
+	if tx.To == "" {
 		// This is a contract creation tx
 		pTx = &d.Transaction{
-			Hash:      tx.Hash().Hex(),
-			From:      sender.Hex(),
-			Contract:  receipt.ContractAddress.Hex(),
-			Value:     tx.Value().String(),
-			Data:      tx.Data(),
-			Gas:       tx.Gas(),
-			GasPrice:  tx.GasPrice().String(),
-			Cost:      tx.Cost().String(),
-			Nonce:     tx.Nonce(),
-			State:     receipt.Status,
-			BlockHash: receipt.BlockHash.Hex(),
+			Hash:      tx.Hash,
+			From:      tx.From,
+			Contract:  tx.Contract,
+			Value:     tx.Value,
+			Data:      tx.Data,
+			Gas:       tx.Gas,
+			GasPrice:  tx.GasPrice,
+			Cost:      tx.Cost,
+			Nonce:     tx.Nonce,
+			State:     tx.State,
+			BlockHash: tx.BlockHash,
 		}
 	} else {
 		// This is a normal tx, so we keep contract field empty
 		pTx = &d.Transaction{
-			Hash:      tx.Hash().Hex(),
-			From:      sender.Hex(),
-			To:        tx.To().Hex(),
-			Value:     tx.Value().String(),
-			Data:      tx.Data(),
-			Gas:       tx.Gas(),
-			GasPrice:  tx.GasPrice().String(),
-			Cost:      tx.Cost().String(),
-			Nonce:     tx.Nonce(),
-			State:     receipt.Status,
-			BlockHash: receipt.BlockHash.Hex(),
+			Hash:      tx.Hash,
+			From:      tx.From,
+			To:        tx.To,
+			Value:     tx.Value,
+			Data:      tx.Data,
+			Gas:       tx.Gas,
+			GasPrice:  tx.GasPrice,
+			Cost:      tx.Cost,
+			Nonce:     tx.Nonce,
+			State:     tx.State,
+			BlockHash: tx.BlockHash,
 		}
 	}
 
@@ -77,24 +75,20 @@ func PublishTx(blockNumber uint64, tx *types.Transaction, sender common.Address,
 
 }
 
-// PublishEvents - Publishing event/ log entries to redis pub-sub topic, to be captured by subscribers
+// PublishEvent - Publishing event/ log entry to redis pub-sub topic, to be captured by subscribers
 // and sent to client application, who are interested in this piece of data
 // after applying filter
-func PublishEvents(blockNumber uint64, receipt *types.Receipt, redis *d.RedisInfo) {
+func PublishEvent(blockNumber uint64, event *db.Events, redis *d.RedisInfo) {
 
-	for _, v := range receipt.Logs {
-
-		if err := redis.Client.Publish(context.Background(), "event", &d.Event{
-			Origin:          v.Address.Hex(),
-			Index:           v.Index,
-			Topics:          c.StringifyEventTopics(v.Topics),
-			Data:            v.Data,
-			TransactionHash: v.TxHash.Hex(),
-			BlockHash:       v.BlockHash.Hex(),
-		}).Err(); err != nil {
-			log.Print(color.Red.Sprintf("[!] Failed to publish event from block %d : %s", blockNumber, err.Error()))
-		}
-
+	if err := redis.Client.Publish(context.Background(), "event", &d.Event{
+		Origin:          event.Origin,
+		Index:           event.Index,
+		Topics:          event.Topics,
+		Data:            event.Data,
+		TransactionHash: event.TransactionHash,
+		BlockHash:       event.BlockHash,
+	}).Err(); err != nil {
+		log.Print(color.Red.Sprintf("[!] Failed to publish event from block %d : %s", blockNumber, err.Error()))
 	}
 
 }
