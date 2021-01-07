9c0ba5cc0b92b779154c6581fe781b746c6feafa
Anjan Roy <anjanroy@yandex.com>
2021-01-07 23:10:32 +0530

    default transaction wrapping by gorm overridden, from now on whenever this behaviour is desired it needs to be manually enabled

--- app/db/db.go
+++ app/db/db.go
@@ -12,7 +12,13 @@ import (
 
 // Connect - Connecting to postgresql database
 func Connect() *gorm.DB {
-	_db, err := gorm.Open(postgres.Open(fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", cfg.Get("DB_USER"), cfg.Get("DB_PASSWORD"), cfg.Get("DB_HOST"), cfg.Get("DB_PORT"), cfg.Get("DB_NAME"))), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)})
+	_db, err := gorm.Open(postgres.Open(fmt.Sprintf("postgresql://%s:%s@%s:%s/%s",
+		cfg.Get("DB_USER"), cfg.Get("DB_PASSWORD"), cfg.Get("DB_HOST"),
+		cfg.Get("DB_PORT"), cfg.Get("DB_NAME"))),
+		&gorm.Config{
+			Logger:                 logger.Default.LogMode(logger.Silent),
+			SkipDefaultTransaction: true, // all db writing to be wrapped inside transaction manually
+		})
 	if err != nil {
 		log.Fatalf("[!] Failed to connect to db : %s\n", err.Error())
 	}
