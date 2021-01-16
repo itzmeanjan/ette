8da2dc84f999020dc06d590e37a81384e4ff485b
Anjan Roy <anjanroy@yandex.com>
2021-01-16 10:18:58 +0530

    upsert transaction entry on conflicting txhash

--- app/db/transaction.go
+++ app/db/transaction.go
@@ -4,7 +4,25 @@ import (
 	"errors"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
+
+// UpsertTransaction - It may be the case previously this block was processed
+// and transaction also got persisted into database, which has been updated
+// on chain, due to chain reorganization
+//
+// Now we want to persist latest entry into database, using upsert operation
+// i.e. if entry doesn't exist yet, it'll be created, but if it does
+// i.e. conflicting primary key found, then, all fields will be updated to latest value
+func UpsertTransaction(dbWTx *gorm.DB, tx *Transactions) error {
+
+	if tx == nil {
+		return errors.New("Empty transaction received while attempting to persist")
+	}
+
+	return dbWTx.Clauses(clause.OnConflict{UpdateAll: true}).Create(tx).Error
+
+}
 
 // StoreTransaction - Stores specific tx into database, where presence of tx
 // in database being performed using db handle ( i.e. `dbWOTx` ) which is not
