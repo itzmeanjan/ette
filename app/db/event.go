a514e8d573bc8e0a2c65f7edf7befb9547550e63
Anjan Roy <anjanroy@yandex.com>
2021-01-16 10:16:15 +0530

    upsert event data, on conflict

--- app/db/event.go
+++ app/db/event.go
@@ -4,7 +4,25 @@ import (
 	"errors"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
+
+// UpsertEvent - It may be the case previously this block was processed
+// and event log also got persisted into database, which has been updated
+// on chain, due to chain reorganization
+//
+// Now we want to persist latest entry into database, using upsert operation
+// i.e. if entry doesn't exist yet, it'll be created, but if it does
+// i.e. conflicting primary key found, then, all fields will be updated to latest value
+func UpsertEvent(dbWTx *gorm.DB, event *Events) error {
+
+	if event == nil {
+		return errors.New("Empty event received while attempting to persist")
+	}
+
+	return dbWTx.Clauses(clause.OnConflict{UpdateAll: true}).Create(event).Error
+
+}
 
 // StoreEvent - Either creating new event entry or updating existing one
 // depending upon whether entry exists already or not
