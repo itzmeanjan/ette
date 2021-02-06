7a497f93e5b340bd6f1569bd33d5fb15e9f7aa92
Anjan Roy <anjanroy@yandex.com>
2021-02-06 20:42:43 +0530

    example showing single websocket connection being used for subscribing to multiple topucs

---
+++ example/all_in_one.js
@@ -0,0 +1,37 @@
+const { client } = require('websocket')
+
+const _client = new client()
+
+let state = true
+
+_client.on('connectFailed', e => { console.error(`[!] Failed to connect : ${e}`); process.exit(1); })
+
+// connect for listening to any block being mined
+// event & any transaction being mined in any of those blocks
+// & any event being emitted from contract interaction transactions
+_client.on('connect', c => {
+    c.on('close', d => {
+        console.log(`[!] Closed connection : ${d}`)
+        process.exit(0)
+    })
+
+    // receiving json encoded message
+    c.on('message', d => { console.log(JSON.parse(d.utf8Data)) })
+
+    // periodic subscription & unsubscription request performed
+    handler = _ => {
+
+        c.send(JSON.stringify({ name: 'block', type: state ? 'subscribe' : 'unsubscribe', apiKey: '0x43ee4523fc8cc569b492fcda6f32e07d90d2afdf7a8a02b5e74c933be1785487' }))
+        c.send(JSON.stringify({ name: 'transaction/*/*', type: state ? 'subscribe' : 'unsubscribe', apiKey: '0x43ee4523fc8cc569b492fcda6f32e07d90d2afdf7a8a02b5e74c933be1785487' }))
+        c.send(JSON.stringify({ name: 'event/*/*/*/*/*', type: state ? 'subscribe' : 'unsubscribe', apiKey: '0x43ee4523fc8cc569b492fcda6f32e07d90d2afdf7a8a02b5e74c933be1785487' }))
+
+        state = !state
+
+    }
+
+    setInterval(handler, 10000)
+    handler()
+
+})
+
+_client.connect('ws://localhost:7000/v1/ws', null)
