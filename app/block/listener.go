626998012db674d69ce800a78d6a292089331340
Anjan Roy <anjanroy@yandex.com>
2021-02-06 21:55:32 +0530

    fixed pubsub test script name

--- app/block/listener.go
+++ app/block/listener.go
@@ -101,7 +101,7 @@ func SubscribeToNewBlocks(connection *d.BlockChainNodeConnection, _db *gorm.DB, 
 					// i.e. trying to fill up gap, which was caused when `ette` was offline
 					//
 					// Backward traversal mechanism gives us more recent blockchain happenings to cover
-					// go SyncBlocksByRange(connection.RPC, _db, redis, header.Number.Uint64()-1, status.MaxBlockNumberAtStartUp(), status)
+					go SyncBlocksByRange(connection.RPC, _db, redis, header.Number.Uint64()-1, status.MaxBlockNumberAtStartUp(), status)
 
 				}
 				// Making sure that when next latest block header is received, it'll not
