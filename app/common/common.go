4cf2db2fa3521dd087b47daf6e85e830f847761d
Anjan Roy <anjanroy@yandex.com>
2021-02-08 19:24:04 +0530

    create event topic map from event topic array ( only valid ones )

--- app/common/common.go
+++ app/common/common.go
@@ -14,3 +14,30 @@ func StringifyEventTopics(data []common.Hash) []string {
 
 	return buffer
 }
+
+// CreateEventTopicMap - Given array of event topics, returns map
+// of valid event topics, to be used when performing selective field based
+// queries on event topics
+func CreateEventTopicMap(topics ...string) map[uint8]string {
+
+	_topics := make(map[uint8]string)
+
+	if topics[0] != "" {
+		_topics[0] = topics[0]
+	}
+
+	if topics[1] != "" {
+		_topics[1] = topics[1]
+	}
+
+	if topics[2] != "" {
+		_topics[2] = topics[2]
+	}
+
+	if topics[3] != "" {
+		_topics[3] = topics[3]
+	}
+
+	return _topics
+
+}
