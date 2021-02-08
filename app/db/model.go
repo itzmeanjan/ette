ca377ca7978326529bcfcec4d55c8396491536fa
Anjan Roy <anjanroy@yandex.com>
2021-02-08 14:33:10 +0530

    gin index set up in migration file i.e. in db model definition

--- app/db/model.go
+++ app/db/model.go
@@ -86,7 +86,7 @@ type Events struct {
 	BlockHash       string         `gorm:"column:blockhash;type:char(66);not null;primaryKey"`
 	Index           uint           `gorm:"column:index;type:integer;not null;primaryKey"`
 	Origin          string         `gorm:"column:origin;type:char(42);not null;index"`
-	Topics          pq.StringArray `gorm:"column:topics;type:text[];not null"`
+	Topics          pq.StringArray `gorm:"column:topics;type:text[];not null;index:,type:gin"`
 	Data            []byte         `gorm:"column:data;type:bytea"`
 	TransactionHash string         `gorm:"column:txhash;type:char(66);not null;index"`
 }
