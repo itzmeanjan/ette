7ff442d8e4267c0395fb46d2f90bc2feeda18311
Anjan Roy <anjanroy@yandex.com>
2021-02-09 18:28:34 +0530

    struct for keeping locking info

--- app/block/lock.go
+++ app/block/lock.go
@@ -4,19 +4,69 @@ import "sync"
 
 // ProcessQueueLock - ...
 type ProcessQueueLock struct {
-	RunningQueue     map[uint64]<-chan bool
-	RunningQueueLock *sync.RWMutex
-	WaitingQueue     map[uint64]chan bool
-	WaitingQueueLock *sync.RWMutex
+	runningQueue     map[uint64]<-chan bool
+	runningQueueLock *sync.RWMutex
+	waitingQueue     map[uint64]chan bool
+	waitingQueueLock *sync.RWMutex
+	done             <-chan uint64
+	wait             <-chan bool
+}
+
+// LockRequest - When some go routines wants to start processing one
+// block, they will send a request of this form i.e. invoke `Acquire`
+// function with these pieces of data
+//
+// Where block number is what they want to process & communication channel
+// is what to be used for communication between that go routine & lock manager
+type LockRequest struct {
+	Block         uint64
+	Communication chan bool
+}
+
+// WaitingQueueWatceher - ...
+func (p *ProcessQueueLock) WaitingQueueWatceher() {
+
+	for {
+
+		select {
+
+		case <-p.done:
+
+		case <-p.wait:
+
+		}
+
+	}
+
 }
 
 // Acquire - ...
-func (p *ProcessQueueLock) Acquire(block uint64, comm chan bool) {
+func (p *ProcessQueueLock) Acquire(block uint64, comm chan bool) bool {
 
-	_, ok := p.RunningQueue[block]
+	_, ok := p.runningQueue[block]
 	if !ok {
 
+		// -- Accessing critical section of code, acquiring lock
+		p.runningQueueLock.Lock()
+
+		p.runningQueue[block] = comm
+
+		p.runningQueueLock.Unlock()
+		// -- Released lock, done with critical section of code
+
+		go p.Running(block, comm)
+		return true
+
 	}
+	// -- Accessing critical section of code, acquiring lock
+	p.waitingQueueLock.Lock()
+
+	p.waitingQueue[block] = comm
+
+	p.waitingQueueLock.Unlock()
+	// -- Released lock, done with critical section of code
+
+	return false
 
 }
 
@@ -26,22 +76,14 @@ func (p *ProcessQueueLock) Running(block uint64, comm <-chan bool) {
 	defer func() {
 
 		// -- Critical section of code, below
-		p.RunningQueueLock.Lock()
+		p.runningQueueLock.Lock()
 
-		delete(p.RunningQueue, block)
+		delete(p.runningQueue, block)
 
-		p.RunningQueueLock.Unlock()
+		p.runningQueueLock.Unlock()
 		// -- Releasing lock, done with critical section of code
 
 	}()
-
-	// -- Accessing critical section of code, acquiring lock
-	p.RunningQueueLock.Lock()
-
-	p.RunningQueue[block] = comm
-
-	p.RunningQueueLock.Unlock()
-	// -- Released lock, done with critical section of code
 
 	// this is a blocking call, only to be unblocked when
 	// client has done it's job & call release lock
