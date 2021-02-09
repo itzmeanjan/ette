161b09b0c30931d8110c5815766ca5a04b6d6878
Anjan Roy <anjanroy@yandex.com>
2021-02-09 18:05:28 +0530

    lock release implementation

--- app/block/lock.go
+++ app/block/lock.go
@@ -4,7 +4,7 @@ import "sync"
 
 // ProcessQueueLock - ...
 type ProcessQueueLock struct {
-	RunningQueue     map[uint64]chan bool
+	RunningQueue     map[uint64]<-chan bool
 	RunningQueueLock *sync.RWMutex
 	WaitingQueue     map[uint64]chan bool
 	WaitingQueueLock *sync.RWMutex
@@ -21,7 +21,7 @@ func (p *ProcessQueueLock) Acquire(block uint64, comm chan bool) {
 }
 
 // Running - ...
-func (p *ProcessQueueLock) Running(block uint64, comm chan bool) {
+func (p *ProcessQueueLock) Running(block uint64, comm <-chan bool) {
 
 	defer func() {
 
@@ -43,10 +43,18 @@ func (p *ProcessQueueLock) Running(block uint64, comm chan bool) {
 	p.RunningQueueLock.Unlock()
 	// -- Released lock, done with critical section of code
 
+	// this is a blocking call, only to be unblocked when
+	// client has done it's job & call release lock
 	<-comm
 	return
 
 }
 
-// Release - ...
-func (p *ProcessQueueLock) Release() {}
+// Release - When done with processing block, worker invokes method
+// to let run tracker know it's done with processing this block, it there's
+// any other worker waiting for processing same block, it can now go in
+func (p *ProcessQueueLock) Release(comm chan<- bool) {
+
+	comm <- true
+
+}
