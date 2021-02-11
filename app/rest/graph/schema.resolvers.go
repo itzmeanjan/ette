c485828998b86967900b02598fc8d465a18deeaf
Anjan Roy <anjanroy@yandex.com>
2021-02-11 21:03:02 +0530

    graphql implementation for tx count in block number range between addresses

--- app/rest/graph/schema.resolvers.go
+++ app/rest/graph/schema.resolvers.go
@@ -273,7 +273,32 @@ func (r *queryResolver) TransactionsToAccountByTimeRange(ctx context.Context, ac
 }
 
 func (r *queryResolver) TransactionCountBetweenAccountsByNumberRange(ctx context.Context, fromAccount string, toAccount string, from string, to string) (int, error) {
-	panic(fmt.Errorf("not implemented"))
+	if !(strings.HasPrefix(fromAccount, "0x") && len(fromAccount) == 42) {
+		return 0, errors.New("Bad From Account Address")
+	}
+
+	if !(strings.HasPrefix(toAccount, "0x") && len(toAccount) == 42) {
+		return 0, errors.New("Bad To Account Address")
+	}
+
+	_from, _to, err := cmn.RangeChecker(from, to, cfg.GetBlockNumberRange())
+	if err != nil {
+		return 0, errors.New("Bad Block Number Range")
+	}
+
+	count := int(_db.GetTransactionCountBetweenAccountsByBlockNumberRange(db, common.HexToAddress(fromAccount), common.HexToAddress(toAccount), _from, _to))
+
+	// Attempting to calculate byte form of number
+	// so that we can keep track of how much data was transferred
+	// to client
+	_count := make([]byte, 4)
+	binary.LittleEndian.PutUint32(_count, uint32(count))
+
+	if err := doBookKeeping(ctx, _count); err != nil {
+		return 0, errors.New("Book keeping failed")
+	}
+
+	return count, nil
 }
 
 func (r *queryResolver) TransactionsBetweenAccountsByNumberRange(ctx context.Context, fromAccount string, toAccount string, from string, to string) ([]*model.Transaction, error) {
