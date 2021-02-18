e25e55da3fb9b4bdc1a262a50869e2b8ac0ae171
Anjan Roy <anjanroy@yandex.com>
2021-02-18 14:45:25 +0530

    safely increment & decrement count

--- app/data/stat.go
+++ app/data/stat.go
@@ -1,7 +1,19 @@
 package data
 
+import "sync/atomic"
+
 // ActiveSubscriptions - Keeps track of how many active websocket
 // connections being maintained now by `ette`
 type ActiveSubscriptions struct {
 	Count uint64
+}
+
+// Increment - Safely increment count by `X`
+func (a *ActiveSubscriptions) Increment(by uint64) {
+	atomic.AddUint64(&a.Count, by)
+}
+
+// Decrement - Safely decrement count by `X`
+func (a *ActiveSubscriptions) Decrement(by uint64) {
+	atomic.AddUint64(&a.Count, ^uint64(by-1))
 }
